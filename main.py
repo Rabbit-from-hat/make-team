8aeae4efa03c4b37b5d20e35caef0e56d5317264
N.Okamoto <magicianchild1412@gmail.com>
2019-08-27 23:17:49 +0900

    make file

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+
+token = "NjEyNDk2ODkzMjg0NzEyNDYz.XVjQ_g.yOWlQWZZ_vfKyDd_thWk6X5HiHI"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    """起動時に通知してくれる処理"""
+    print('-----Logged in info-----')
+    print(client.user.name)
+    print(client.user.id)
+    print(discord.__version__)
+    print('------------------------')
+
+@client.event
+async def on_message(message):
+    """メッセージを処理"""
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+            
+
+# botの接続と起動
+client.run(token)
