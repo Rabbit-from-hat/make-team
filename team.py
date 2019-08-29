1db45e0855c3b794d1f4fa00fbbdae0026800bae
N.Okamoto <magicianchild1412@gmail.com>
2019-08-29 20:14:49 +0900

    bugfix

--- team.py
+++ team.py
@@ -4,5 +4,4 @@ import random
 def team_shuffle(message):
         channel_mem = [i.name for i in message.author.voice.channel.members] #VCメンバリスト取得
         random.shuffle(channel_mem)
-        await message.channel.send("\n".join(channel_mem[::2]))
-        await message.channel.send("\n".join(channel_mem[1::2]))
+        await message.channel.send("\n".join(channel_mem))
