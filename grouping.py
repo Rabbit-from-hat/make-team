1727970981dec10345d65d41cd0e3bbd23166a90
N.Okamoto <magicianchild1412@gmail.com>
2019-08-31 01:54:11 +0900

    チーム数の指定確認追記

--- grouping.py
+++ grouping.py
@@ -2,8 +2,12 @@ import discord
 from discord.ext import commands
 import random
 
-def default_make(ctx, party_num):
-    state = ctx.author.voice
+def default_make(ctx, *party_num):
+
+    if not party_num: #引数が指定されていない場合
+        return 'チーム数を指定してください。'
+
+    state = ctx.author.voice #VCのステータスを取得
 
     if state is None: #依頼主自身がVoiceChannelにいないとき
         return '実行できません。ボイスチャンネルに入ってコマンドを実行してください。'
