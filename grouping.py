be57e72e41b0e892d19fd94d5e82ab8da0cac272
N.Okamoto <magicianchild1412@gmail.com>
2019-09-01 16:52:10 +0900

    参加人数が1人だけの場合の対処

--- grouping.py
+++ grouping.py
@@ -7,7 +7,8 @@ def default_make(ctx, party_num):
     remainder = []
     state = ctx.author.voice #VCのステータスを取得
 
-    if state is None: #依頼主自身がVoiceChannelにいないとき
+    #依頼主自身がVoiceChannelにいないとき
+    if state is None: 
         return '実行できません。ボイスチャンネルに入ってコマンドを実行してください。'
 
     channel_mem = [i.name for i in state.channel.members] #VCメンバリスト取得
@@ -15,15 +16,20 @@ def default_make(ctx, party_num):
     random.shuffle(channel_mem)
     mem_len = len(channel_mem)
 
-    #VCの人数が割り切れない場合
-    #remainder_num = mem_len % party_num
-    #if remainder_num != 0: 
-    #    for r in range(remainder_num):
-    #        remainder.append(channel_mem.pop())
-    #    team.append("余剰")
-    #    team.extend(remainder)
+    #ボイスチャンネルに一人しかいない場合
+    if mem_len == 1:
+        return '実行できません。２人以上参加してください。'
 
-    for i in range(party_num): #チーム分け
+    #VCの人数が割り切れない場合
+    remainder_num = mem_len % party_num
+    if remainder_num != 0: 
+        for r in range(remainder_num):
+            remainder.append(channel_mem.pop())
+        team.append("余剰")
+        team.extend(remainder)
+
+    #チーム分け
+    for i in range(party_num): 
         team.append("チーム"+str(i+1))
         team.extend(channel_mem[i:mem_len:party_num])
 
