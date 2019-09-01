1e0ceca1f0a22f40cf185b04fc30a9bc8647b2ff
N.Okamoto <magicianchild1412@gmail.com>
2019-09-01 16:40:13 +0900

    テスト

--- grouping.py
+++ grouping.py
@@ -16,14 +16,15 @@ def default_make(ctx, party_num):
     mem_len = len(channel_mem)
 
     #VCの人数が割り切れない場合
-    remainder_num = mem_len % party_num
-    if remainder_num != 0: 
-        for r in range(remainder_num):
-            remainder.append(channel_mem.pop())
-        team.append("余剰")
-        team.extend(remainder)
+    #remainder_num = mem_len % party_num
+    #if remainder_num != 0: 
+    #    for r in range(remainder_num):
+    #        remainder.append(channel_mem.pop())
+    #    team.append("余剰")
+    #    team.extend(remainder)
 
     for i in range(party_num): #チーム分け
+        team.append("チーム"+str(i+1))
         team.extend(channel_mem[i:mem_len:party_num])
 
     return team
