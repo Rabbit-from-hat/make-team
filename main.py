5663f8aac19da5891fa13f17c6f11b6d43566e96
N.Okamoto <magicianchild1412@gmail.com>
2019-09-29 12:34:22 +0900

    ver.2.0

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-import discord
-from discord.ext import commands
 import os
 import traceback
-import grouping
+
+import discord
+from discord.ext import commands
+
+from action import grouping
+from action import validations
 
 token = os.environ['DISCORD_BOT_TOKEN']
 bot = commands.Bot(command_prefix='/')
+validation = validations.Validations()
 
+"""起動処理"""
 @bot.event
 async def on_ready():
-    """起動処理"""
     print('-----Logged in info-----')
     print(bot.user.name)
     print(bot.user.id)
     print(discord.__version__)
     print('------------------------')
 
+"""コマンド実行"""
 @bot.command()
+@validation.int_check
 async def team(ctx, party_num): #チーム作成
-    msg = grouping.default_make(ctx, party_num)
-    await ctx.channel.send(msg)
-            
-# botの接続と起動
+    g = grouping.Grouping(ctx, party_num)
+    await g.default_make()
+
+"""botの接続と起動"""
 bot.run(token)
